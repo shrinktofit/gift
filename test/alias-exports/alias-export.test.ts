5f7ddcc89cdaca932fdcd6235362fc04bf0fa875
shrinktofit <leslieleigh@outlook.com>
2021-04-08 16:50:23 +0800

    Release 3.1.0; Add named tuple member support

--- test/alias-exports/alias-export.test.ts
+++ test/alias-exports/alias-export.test.ts
@@ -1,21 +1,21 @@
 
 import ps from 'path';
 import fs from 'fs-extra';
-import { bundle, GiftErrors } from '../../source/gift';
+import { bundle } from '../../source/gift';
 
 describe('alias export', () => {
     const match = async (dirName: string) => {
         const inputPath = ps.join(__dirname, dirName, 'input.d.ts');
         const outputPath = ps.join(__dirname, dirName, 'output.d.ts');
-        const { code, error } = bundle({
+        const result = bundle({
             input: inputPath,
             rootModule: 'index',
             name: 'out/index',
             output: outputPath,
         });
-        expect(error).toStrictEqual(GiftErrors.Ok);
+        expect(result.groups.length).toBe(1);
         const expectedCode = await fs.readFile(outputPath, 'utf8');
-        expect(code).toStrictEqual(expectedCode);
+        expect(result.groups[0].code).toStrictEqual(expectedCode);
     };
 
     // Export from namespace to module
